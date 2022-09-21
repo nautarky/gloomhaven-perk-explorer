ec27eb46315538e9744f2f16e196e8906c5a2eed
Matt D <nautarkygg@gmail.com>
2022-09-20 23:42:25 -0700

    Polish class component

--- components/ClassSelectForm.tsx
+++ components/ClassSelectForm.tsx
@@ -1,29 +1,27 @@
 import React, { FunctionComponent, useState } from "react";
 import { Listbox } from "@headlessui/react";
 
-const classNames = ["Inox Brute", "Quatryl Tinkerer", "Human Scoundrel"];
+import { GloomhavenClass } from "../types/gloomhaven";
 
 const ClassSelectForm: FunctionComponent = () => {
-  const [selectedClassName, setSelectedClassName] = useState(classNames[0]);
+  const [selectedClassName, setSelectedClassName] = useState(GloomhavenClass.InoxBrute);
 
   return (
-    <div className="w-72 mt-1 ml-1">
-      <h1>Class selection:</h1>
+    <div className="w-72 mt-1 ml-1 border pl-1 pr-1 pt-1 rounded-lg h-20">
+      <h1 className="pl-3">Class</h1>
       <Listbox value={selectedClassName} onChange={setSelectedClassName}>
-        <Listbox.Button className="relative w-full bg-white rounded-lg shadow-md text-left py-1 pl-3 pr-10 border">
+        <Listbox.Button className="relative w-full bg-white rounded-lg shadow-md text-left py-1 pl-3 border">
           {selectedClassName}
         </Listbox.Button>
         <Listbox.Options className="mt-1 w-full bg-white rounded-lg shadow-md border">
-          {classNames.map((name) => (
+          {Object.keys(GloomhavenClass).map((ghClass) => (
             <Listbox.Option
               as="div"
-              key={name}
-              value={name}
-              className={({ active }) =>
-                `cursor-pointer pl-3 py-1 ${active ? "bg-amber-100" : ""}`
-              }
+              key={GloomhavenClass[ghClass as keyof typeof GloomhavenClass]}
+              value={GloomhavenClass[ghClass as keyof typeof GloomhavenClass]}
+              className={({ active }) => `cursor-pointer pl-3 py-1 ${active ? "bg-amber-100" : ""}`}
             >
-              {name}
+              {GloomhavenClass[ghClass as keyof typeof GloomhavenClass]}
             </Listbox.Option>
           ))}
         </Listbox.Options>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
-import type { NextPage } from "next";
+import { NextPage } from "next";
+import React from "react";
 
 import ClassSelectForm from "../components/ClassSelectForm";
 
-const Home: NextPage = () => {
-  return (
-    <div className="bg-amber-300 w-full h-8">
-      <h1 className="text-2xl pl-3">Gloomhaven Perk Explorer</h1>
-      <ClassSelectForm />
-    </div>
-  );
-};
+const Home: NextPage = () => (
+  <div className="bg-amber-300 w-full h-8">
+    <h1 className="text-2xl pl-3">Gloomhaven Perk Explorer</h1>
+    <ClassSelectForm />
+  </div>
+);
 
 export default Home;
